fda08f2c2cdd62ee847372ab06f6f2810e49b6ff
es5es5 <es5e5s@nate.com>
2021-02-07 21:00:52 +0900

    Material Icons

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,55 @@ class MyApp extends StatelessWidget {
                     fontSize: 40,
                     color: Colors.white,
                     fontWeight: FontWeight.bold),
-              )
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Quicksand',
+                  fontSize: 20,
+                  color: Colors.teal[100],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+82 010 5555 4444',
+                      style: TextStyle(color: Colors.teal[900], fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'louis_lee@gmail.com',
+                      style: TextStyle(color: Colors.teal[900], fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
